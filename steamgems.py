4795211b4affa4c50524d6e528a5299a96e55e64
Jan Vollmer <jan@vllmr.dev>
2022-11-08 18:55:12 +0100

    [steamgems] get most recent gem value from steam api

--- steamgems.py
+++ steamgems.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
-from steam.webauth import WebAuth as SteamClient
-from contextlib import contextmanager
+
 import typing as t
-from rich.progress import Progress, MofNCompleteColumn, BarColumn, TextColumn
-import time
+from contextlib import contextmanager
+from urllib.parse import urlencode
+
+from rich.progress import MofNCompleteColumn, Progress
+from steam.webauth import WebAuth as SteamClient
 
 if t.TYPE_CHECKING:
     import requests
@@ -24,7 +26,7 @@ def steam_client():
 
 def get_items(session: requests.Session, steamid64: str):
     resp = session.get(
-        f"https://steamcommunity.com/inventory/{steamid64}/{STEAM_ITEMS_SECTION_ID}/{INVENTORY_CONTEXT_ID}?count={ITEMS_IN_INVENTORY_COUNT}"
+        f"https://steamcommunity.com/inventory/{steamid64}/{STEAM_ITEMS_SECTION_ID}/{INVENTORY_CONTEXT_ID}?count={ITEMS_IN_INVENTORY_COUNT}"  # noqa: E501
     )
     if resp.status_code != 200:
         raise Exception("Inventory could not be loaded")
@@ -48,14 +50,6 @@ def get_item_description(items: dict[str, t.Any], classid: str):
             return item
 
 
-def extract_expected_gems(js_fn: str):
-    try:
-        return js_fn.split(",")[3].strip()
-    except IndexError as exc:
-        print(js_fn)
-        raise exc
-
-
 def main():
     with steam_client() as client:
         steamid64 = client.steam_id
@@ -76,17 +70,19 @@ def main():
                 if "owner_actions" in item_desc and "market_fee_app" in item_desc:
                     if "goo" not in item_desc["owner_actions"][-1]["link"].lower():
                         continue
-                    json = {
+                    data = {
                         "contextid": str(INVENTORY_CONTEXT_ID),
                         "assetid": assetid,
                         "sessionid": session_id,
-                        "goo_value_expected": extract_expected_gems(
-                            item_desc["owner_actions"][-1]["link"]
-                        ),
                         "appid": str(item_desc["market_fee_app"]),
                     }
+
+                    data["goo_value_expected"] = session.get(
+                        f"https://steamcommunity.com/id/{STEAM_PROFILE_ID}/ajaxgetgoovalue?{urlencode(data)}"  # noqa: E501
+                    ).json()["goo_value"]
+
                     headers = {
-                        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0",
+                        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0",  # noqa: E501
                         "X-Requested-With": "XMLHttpRequest",
                         "Referer": f"https://steamcommunity.com/id/{STEAM_PROFILE_ID}/inventory/",
                         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
@@ -96,13 +92,12 @@ def main():
 
                     resp = session.post(
                         f"https://steamcommunity.com/id/{STEAM_PROFILE_ID}/ajaxgrindintogoo/",
-                        data=json,
+                        data=data,
                         headers=headers,
                     )
-                    assert resp.status_code in (200, 500), (resp.status_code, resp.text)
+                    assert resp.status_code == 200, (resp.status_code, resp.text, data)
                     completed += 1
                     progress.update(task, completed=completed)
-                    time.sleep(1)
 
 
 if __name__ == "__main__":
